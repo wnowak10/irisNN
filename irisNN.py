abb66a2866dd28dfdee7f2a7bf61969ffd2861fe
wnowak <wnowak10@gmail.com>
2016-10-06 21:32:41 -0400

    cleaned up file. working, but not as fast as kaggle.

--- irisNN.py
+++ irisNN.py
@@ -12,63 +12,68 @@ iris = iris.drop('Id', 1)
 # this is super convenient. pops the target column from train set.  and creates target
 target = iris.pop("Species")
 
+# converts from dataframe to np array
 ir=iris.values
 
-
+# convert train labels to one hots
 train_labels = pd.get_dummies(target)
+# make np array
 trrr=train_labels.values
 
 X_train,X_test,y_train,y_test = train_test_split(ir,trrr,test_size=0.2)
 # # so no we have predictors and y values, separated into test and train
-y_train
 
-
-
-# print(X_train.shape)
 
 # there are four descriptor variables
+# place holder for inputs. feed in later
 x = tf.placeholder(tf.float32, [None, 4])
+
+# initialize variables
 # # take 4 descriptors to one of 3 outputs
 W = tf.Variable(tf.zeros([4, 3]))
 # # add biases for each output
 b = tf.Variable(tf.zeros([3]))
 
-# #implement model. these are predicteds
+
+# #implement model. these are predicted ys
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-# #hold correct results
-y_ = tf.placeholder(tf.float32, [None, 3])
+
+# placeholder for y values 
+tf_softmax_correct = tf.placeholder("float", [None,3])
+
+
 
 # CE loss
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(tf_softmax_correct * tf.log(y), reduction_indices=[1]))
 # GD training
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 
-# this is where preicted y's go
-tf_softmax_correct = tf.placeholder("float", [None,3])
-
-
+# check accuracy
 tf_correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(tf_softmax_correct,1))
 tf_accuracy = tf.reduce_mean(tf.cast(tf_correct_prediction, "float"))
 
+
+
+
 # # init all vars
 init = tf.initialize_all_variables()
-
-
-
+# start session
 sess = tf.Session()
 sess.run(init)
 
 
-# this works
+
 k=[]
 for i in range(1000):
-  sess.run(train_step, feed_dict={x: X_train, y_: y_train})
+  sess.run(train_step, feed_dict={x: X_train, tf_softmax_correct: y_train})
 # Print accuracy
   result = sess.run(tf_accuracy, feed_dict={x: X_test, tf_softmax_correct: y_test})
   print("Run {},{}".format(i,result))
   k.append(result)
+  if result == 1:
+  	break
 
 
 result = sess.run(tf_accuracy, feed_dict={x: X_test, 
